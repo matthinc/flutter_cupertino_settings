83daba12a9b06fb7790dd0894f98b198f42f3cf5
tb2706 <bierbaum.tim@outlook.de>
2019-08-26 16:44:06 +0200

    Add CSDescription Widget, add utility function to determine theme brightness

---
+++ lib/widgets/description.dart
@@ -0,0 +1,31 @@
+part of flutter_cupertino_settings;
+
+class CSDescription extends StatelessWidget {
+  final String description;
+  final Color backgroundColorDark;
+
+  const CSDescription(
+    this.description, {
+    this.backgroundColorDark = Colors.black12,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(
+        10,
+        7.5,
+        5,
+        5,
+      ),
+      color: _isDark(context) ? backgroundColorDark : CS_HEADER_COLOR,
+      child: Text(
+        description,
+        style: TextStyle(
+          color: _isDark(context) ? null : CS_HEADER_TEXT_COLOR,
+          fontSize: CS_DESCRIPTION_FONT_SIZE,
+        ),
+      ),
+    );
+  }
+}
