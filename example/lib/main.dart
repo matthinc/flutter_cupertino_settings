0e4a05e7cf5473124b0cbbb6ba19aa83130fa8bc
tb2761 <tb2706@outlook.de>
2020-01-23 20:01:27 +0100

    remove unnessecary material import in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_cupertino_settings/flutter_cupertino_settings.dart';
 
